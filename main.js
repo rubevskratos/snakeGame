87479b787bbe6b7a889b45f201f8d02312346e53
Ruben Carballo <rcarballoq@gmail.com>
2021-11-19 00:51:42 +0100

    added collission with self to end game

--- main.js
+++ main.js
@@ -1,90 +1,139 @@
 
 const SnakeGame = function() {
-    let snakePosition;
-    let mousePosition;
-    
-    let snakeSpeed = 500;
-    let snakeSize;
-
-    let move;
+    let move; //to save the interval.
 
     var snake = {
-        x: 10, 
-        y: 10, 
+        pos: [{x: 10, y: 10}],
+        speed: 500,
+        grow: false,
         direction: 2, //0: up, 1: down, 2: left, 3: right
-        clean: function () {
-            let snakeCells = document.querySelectorAll('.snake');
-            snakeCells.forEach(e => {
-                e.classList.remove('snake');
+
+        update: function() {
+            //move in direction
+            switch (snake.direction) {
+                case 0: 
+                    snake.pos.unshift({
+                        x: snake.pos[0].x,
+                        y: snake.pos[0].y === 1 ? 20 : snake.pos[0].y - 1});
+                    break;
+                case 1: 
+                    snake.pos.unshift({
+                        x: snake.pos[0].x,
+                        y: snake.pos[0].y === 20 ? 1 : snake.pos[0].y + 1});
+                     break;
+                case 2: 
+                    snake.pos.unshift({
+                        x: snake.pos[0].x === 1 ? 20: snake.pos[0].x - 1,
+                        y: snake.pos[0].y});
+                    break;
+                case 3: 
+                    snake.pos.unshift({
+                        x: snake.pos[0].x === 20 ? 1: snake.pos[0].x + 1,
+                        y: snake.pos[0].y});
+                    break;
+            }
+            //check if grows
+            if (snake.grow == true) {
+                snake.grow = false;
+            } else {
+                snake.pos.pop()
+            }
+        },
+
+        draw: function() {
+            snake.pos.forEach(p => {
+                const cell = document.querySelector(`.row${p.y} .col${p.x}`)
+                cell.classList.add('snake')
             })
         },
-        update: function() {
-            switch (snake.direction) {
-                case 0: snake.y = snake.y === 1 ? 20 : this.y - 1; break;
-                case 1: snake.y = snake.y === 20 ? 1 : this.y + 1; break;
-                case 2: snake.x = snake.x === 1 ? 20: this.x - 1; break;
-                case 3: snake.x = snake.x === 20 ? 1: this.x + 1; break;
-            }
-            
 
-        },
-        draw: function() {
-            snakePosition = document.querySelector(`.row${snake.y} .col${snake.x}`)
-            snakePosition.classList.add('snake')
-        },
-        
         eatMouse: function () {
-            if(snake.x === mouse.x && snake.y === mouse.y) {
-                mousePosition.classList.remove('mouse');
-                snake.update();
+            if(snake.pos[0].x === mouse.pos[0].x && snake.pos[0].y === mouse.pos[0].y) {
                 mouse.update();
+                snake.grow=true;
+                snake.update();
                 board.speedUp();
-                console.log(mouse.x)
-                console.log(snakeSpeed)
-                
+                console.log(snake.pos)
             }      
 
         },
         
         move: function() {
-            snake.clean();
+            board.clean();
             snake.update();
             snake.eatMouse();
-            snake.draw();
+            board.draw();
+            board.collision();
+        },
+
+        grow: function() {
+            let currentPos = snake.pos[0]
+            snake.pos.push(currentPos)
         }
     }
     
     var mouse = {
-        x: 5, 
-        y: 5,
+        pos: [{x: 5, y: 5}],
         update: function() {
-            mouse.x = Math.ceil(Math.random() * 20);
-            mouse.y = Math.ceil(Math.random() * 20);
-            mousePosition = document.querySelector(`.row${mouse.y} .col${mouse.x}`)
-            mousePosition.classList.add('mouse')
+            mouse.pos[0].x = Math.ceil(Math.random() * 20);
+            mouse.pos[0].y = Math.ceil(Math.random() * 20);
+            mouse.pos.forEach(p => {
+                const celm = document.querySelector(`.row${mouse.pos[0].y} .col${mouse.pos[0].x}`)
+                celm.classList.add('mouse')})
         }
     }
     
     var board = {
+        clean() {
+            const snakePos = document.querySelectorAll('.snake')
+            snakePos.forEach(elem => { elem.classList.remove('snake') })
+            const mousePos = document.querySelectorAll('.mouse')
+            mousePos.forEach(elem => { elem.classList.remove('mouse') })
+          },
+
         draw: function() {
-            snakePosition = document.querySelector(`.row${snake.y} .col${snake.x}`)
-            snakePosition.classList.add('snake')
-            
-            mousePosition = document.querySelector(`.row${mouse.y} .col${mouse.x}`)
-            mousePosition.classList.add('mouse')
+            snake.pos.forEach(p => {
+                const cels = document.querySelector(`.row${p.y} .col${p.x}`)
+                cels.classList.add('snake')
+            })
+            mouse.pos.forEach(m => {
+                const celm = document.querySelector(`.row${m.y} .col${m.x}`)
+                celm.classList.add('mouse')
+            })
         },
+
         speedUp: function() {
             clearInterval(move);
 
-            if (snakeSpeed <= 100 ) snakeSpeed = 100; else snakeSpeed = 399 / 500 * snakeSpeed;
-            move = setInterval(snake.move,snakeSpeed)
+            if (snake.speed <= 100 ) 
+                snake.speed = 100; 
+            else 
+                snake.speed = 399 / 500 * snake.speed;
+
+            move = setInterval(snake.move,snake.speed)
+        },
+
+        collision: function() {
+            let head = snake.pos[0]
+            //check collission with self.
+            for(var i = 1; i < snake.pos.length; i++) {
+                if (head.x === snake.pos[i].x && head.y === snake.pos[i].y) {
+                    board.gameIsOver();
+                    window.location.reload();
+                }
+            }
+        },
+
+        gameIsOver: function() {
+            window.alert('This is Game Over');
+            window.location.reload()
         }
     }
                         
                     
     this.start = function() {
         board.draw();
-        move = setInterval(snake.move,snakeSpeed)
+        move = setInterval(snake.move,snake.speed)
         window.addEventListener('keydown', e => {
         switch(e.key) {
             case 'ArrowUp': snake.direction = 0; break;
